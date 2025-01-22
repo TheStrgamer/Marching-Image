6e370278460e3fabde4df6f2754fe943c2f9291d
Konrad <konradseime@gmail.com>
2025-01-22 16:04:16 +0100

    Added more methods to ColorMap

--- ColorMap.hpp
+++ ColorMap.hpp
@@ -8,15 +8,20 @@
 class ColorMap {
   public:
     ColorMap();
+    ColorMap(std::vector<Color> colors);
     ColorMap(std::vector<std::string> colors);
-    std::vector<std::string> getColors() const;
-    std::string getColor(int index) const;
-    void addColor(std::string color);
+    void addColor(Color);
+    void addColor(const std::string& color);
+    std::vector<Color> getColors() const;
+    Color getColor(int index) const;
+    void removeColor(const Color& color);
+    void removeColor(const std::string& color);
     void removeColor(int index);
     void clear();
-    std::string getClosestColor(const std::string &color) const;
+    Color getClosestColor(const Color &color) const;
+    Color getClosestColor(const std::string &color) const;
   private:
-    std::vector<std::string> colors;
+    std::vector<Color> colors;
 };
 
 /**
@@ -32,14 +37,27 @@ inline ColorMap::ColorMap() {
  * @param colors The list of colors
  */
 inline ColorMap::ColorMap(const std::vector<std::string> colors) {
-  this->colors = colors;
+  this -> colors = {};
+  for (const auto &color : colors) {
+    const Color c(color);
+    addColor(c);
+  }
+}
+
+/**
+ * @brief Constructor with a list of colors
+ * Initializes the color map with the given list of colors.
+ * @param colors The list of colors
+ */
+inline ColorMap::ColorMap(const std::vector<Color> colors) {
+  this -> colors = colors;
 }
 
 /**
  * @brief Get the list of colors
  * @return The list of colors
  */
-inline std::vector<std::string> ColorMap::getColors() const {
+inline std::vector<Color> ColorMap::getColors() const {
   return colors;
 }
 
@@ -48,7 +66,7 @@ inline std::vector<std::string> ColorMap::getColors() const {
  * @param index The index of the color
  * @return The color
  */
-inline  std::string ColorMap::getColor(const int index) const {
+inline  Color ColorMap::getColor(const int index) const {
   return colors.at(index);
 }
 
@@ -56,17 +74,56 @@ inline  std::string ColorMap::getColor(const int index) const {
  * @brief Add a color to the list
  * @param color The color to add
  */
-inline void ColorMap::addColor(const std::string color) {
+inline void ColorMap::addColor(Color color) {
   colors.emplace_back(color);
+}
+/**
+ * @brief Add a color to the list
+ * @param color The color to add
+ */
+inline void ColorMap::addColor(const std::string& color) {
+  const Color c(color);
+  addColor(c);
 }
 
 /**
  * @brief Remove a color from the list
  * @param index The index of the color to remove
+ * @throws invalid_argument If the index is out of bounds
  */
 inline void ColorMap::removeColor(const int index) {
+  if (index < 0 || index >= colors.size()) {
+    throw std::invalid_argument("Index out of bounds");
+  }
   colors.erase(colors.begin() + index);
 }
+
+/**
+ * @brief Remove a color from the list
+ * @param color The color to remove
+ * @throws invalid_argument If the color is not found
+ */
+inline void ColorMap::removeColor(const Color &color) {
+  for (int i = 0; i < colors.size(); i++) {
+    if (colors.at(i).getHex() == color.getHex()) {
+      removeColor(i);
+      return;
+    }
+  }
+  throw std::invalid_argument("Color not found");
+}
+
+/**
+ * @brief Remove a color from the list
+ * @param color The color to remove
+ * @throws invalid_argument If the color is not found
+ */
+inline void ColorMap::removeColor(const std::string &color) {
+  Color c(color);
+  removeColor(c);
+}
+
+
 
 /**
  * @brief Clear the list of colors
@@ -80,8 +137,18 @@ inline void ColorMap::clear() {
  * @param color The color to compare
  * @return The closest color
  */
-inline std::string ColorMap::getClosestColor(const std::string &color) const {
-  return "";
+inline Color ColorMap::getClosestColor(const Color& color) const {
+  return {};
+}
+
+/**
+ * @brief Get the closest color to the given color
+ * @param color The color to compare
+ * @return The closest color
+ */
+inline Color ColorMap::getClosestColor(const std::string &color) const {
+  Color c(color);
+  return getClosestColor(c);
 }
 
 
