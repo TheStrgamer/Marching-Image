4fd4eaed6564b3f4aedff28cb8d09218066fe19c
Konrad <konradseime@gmail.com>
2026-01-04 18:42:15 +0100

    made a somewhat functional button to swap between server and client side compute

--- frontend/colormap/src/App.tsx
+++ frontend/colormap/src/App.tsx
@@ -1,21 +1,33 @@
 import "./App.css";
 import ImageMapper from "./components/ImageMapper";
 import ColorpickerList from "./components/ColorpickerList";
-import { useState } from "react";
+import { use, useState } from "react";
 function App() {
   const [image, setImage] = useState<string | null>(null);
   const [colors, setColors] = useState(["#FF0000", "#FFFFFF"]);
   const [resultImage, setResultImage] = useState<string | null>(null);
 
+  const [clientOnly, setClientOnly] = useState<boolean>(true);
+
   return (
     <>
-      <h1>Upload image to map it (client-side)</h1>
-      <div className="content">
+      <h1>Upload image to map it {clientOnly && <span> (client-side)</span>}{!clientOnly && <span> (server-side)</span>}</h1>
+      <label className="inline-check">
+        <input
+        type="checkbox"
+            checked={clientOnly}
+            onChange={(e) => setClientOnly(e.target.checked)}
+            style={{width: 12}}
+          />
+          client side
+      </label>      
+        <div className="content">
         <div>
           <h2>Colors</h2>
           <ColorpickerList setColorList={setColors} />
         </div>
-        <ImageMapper setImageParent={setImage} colors={colors} setResultImage={setResultImage}/>
+        {!clientOnly && <h2>Not fully finished yet</h2>}
+        {clientOnly && <ImageMapper setImageParent={setImage} colors={colors} setResultImage={setResultImage}/>}
         </div>
     </>
   );
